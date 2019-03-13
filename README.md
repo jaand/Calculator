d3374ef0fed63b11fcff5539d010132a64c4c847
jaand <kuba.and@op.pl>
2019-03-13 22:23:22 +0100

    Same changes

---
+++ README.md
@@ -0,0 +1,15 @@
+#Calculator
+####This is calculator with basic mathematical operations, which allows you to :
+"+" add
+
+"-"  subtract
+
+"*"  multiply
+
+"/"  divide
+
+#### How to run it:
+1) Install `virtualenvwrapper`
+2) `mkvirtualenv Calculator`
+3) `pip install -r requirements.txt`
+4) run `frontend.py`
--- frontend.py
+++ frontend.py
@@ -1,89 +1,20 @@
-from tkinter import *
-from tkinter import ttk
-
-window = Tk()  # creating main window of program
+from tkinter import ttk, StringVar, END, Tk
 
 
 class Calculator:
-
     calc_value = 0.0  # the value at the beginning
-
-    def button_press(self, value):
-
-        entered_value = self.number_entered.get()  # shows proper value in entry window
-
-        entered_value += value  # writes clicked number next to existing number (
-
-        self.number_entered.delete(0, END)  # deletes current entry window
-
-        self.number_entered.insert(0, entered_value)  # shows value we pressed
-
-    def check_if_float(self, string_value):
-        try:
-            float(string_value)
-            return True
-        except ValueError:
-            return False
-
-    def math_button_press(self, value):
-
-        if self.check_if_float(str(self.number_entered.get())):
-            self.add_trigger = False
-            self.subt_trigger = False
-            self.mult_trigger = False
-            self.div_trigger = False
-
-            self.calc_value = float(self.entered_value.get())
-
-            # lines below exist only for debugging
-            # showing which button was clicked so u can check the correctness
-
-            if value == "/":
-                print("/ Pressed")
-                self.div_trigger = True
-            elif value == "*":
-                print("* Pressed")
-                self.mult_trigger = True
-            elif value == "+":
-                print("+ Pressed")
-                self.add_trigger = True
-            else:
-                print("- Pressed")
-                self.subt_trigger = True
-
-            self.number_entered.delete(0, END)  # deletes entry window after you clicked any of this buttons
-
-    def delete_button_press(self):
-
-        self.number_entered.delete(0, END)
-
-    def equal_button_press(self):
-
-        if self.add_trigger or self.mult_trigger or self.div_trigger or self.subt_trigger:
-
-            if self.add_trigger:
-                solution = self.calc_value + float(self.entered_value.get())  # logic for adding
-            elif self.subt_trigger:
-                solution = self.calc_value - float(self.entered_value.get())  # logic for substraction
-            elif self.mult_trigger:
-                solution = self.calc_value * float(self.entered_value.get())  # logic for multiplication
-            else:
-                try:
-                    solution = self.calc_value / float(self.entered_value.get())  # logic for division
-                except ZeroDivisionError:
-                    print("You can't divide by zero")
-                    return
-
-            print(self.calc_value, " ", float(self.entered_value.get()),
-                  " ", solution)
-
-            self.number_entered.delete(0, END)
-
-            self.number_entered.insert(0, solution)
 
     def __init__(self, window):
 
         self.entered_value = StringVar(window)
+        self.trigger = None
+        self.ZERO_DIVISION_MESSAGE = "You can't divide by zero"
+        self.actions = {
+            "/": self.divide,
+            "+": self.add,
+            "*": self.mult,
+            "-": self.subt,
+        }
 
         window.title("SimpleCalculator")  # define the name/title which appears on program window
         window.geometry("447x220")  # define the size of program window
@@ -97,8 +28,9 @@ class Calculator:
         style.configure("TEntry",
                         font="Dublin 20",
                         padding=10)
-                                                    # below are all existing buttons
-                                                    # their size, "name", and placement
+
+        # below are all existing buttons
+        # their size, "name", and placement
         self.number_entered = ttk.Entry(window,
                                         textvariable=self.entered_value, width=40)
         self.number_entered.grid(row=0, columnspan=3)
@@ -154,7 +86,67 @@ class Calculator:
         self.b_ac = ttk.Button(window, text="AC",
                                command=lambda: self.delete_button_press()).grid(row=4, column=2)
 
+    def button_press(self, value):
 
-Calculator(window)
+        entered_value = self.number_entered.get()  # shows proper value in entry window1
 
-window.mainloop()
+        if entered_value == self.ZERO_DIVISION_MESSAGE:
+            self.number_entered.delete(0, END)
+            self.number_entered.insert(0, value)
+        else:
+            entered_value = self.number_entered.get()
+            entered_value += value  # writes clicked number next to existing number (
+
+            self.number_entered.delete(0, END)  # deletes current entry window
+
+            self.number_entered.insert(0, entered_value)  # shows value we pressed
+
+    def check_if_float(self, string_value):
+        try:
+            float(string_value)
+            return True
+        except ValueError:
+            return False
+
+    def math_button_press(self, value):
+        if self.check_if_float(str(self.number_entered.get())):
+            self.calc_value = float(self.entered_value.get())
+
+            self.trigger = value
+
+            self.number_entered.delete(0, END)  # deletes entry window after you clicked any of this buttons
+
+    def delete_button_press(self):
+        self.number_entered.delete(0, END)
+
+    def add(self):
+        return self.calc_value + float(self.entered_value.get())
+
+    def mult(self):
+        return self.calc_value * float(self.entered_value.get())
+
+    def subt(self):
+        return self.calc_value - float(self.entered_value.get())
+
+    def divide(self):
+        try:
+            return self.calc_value / float(self.entered_value.get())
+        except ZeroDivisionError:
+            print(self.ZERO_DIVISION_MESSAGE)  # only for debugging
+            self.number_entered.delete(0, END)  # deletes all data when trying divide by zero
+            return self.ZERO_DIVISION_MESSAGE
+
+    def equal_button_press(self):
+        equal = self.actions[self.trigger]
+        solution = equal()
+
+        self.number_entered.delete(0, END)
+
+        self.number_entered.insert(0, solution)
+
+
+if __name__ == "__main__":  #
+    window = Tk()  # creating main window of program
+    Calculator(window)
+
+    window.mainloop()
